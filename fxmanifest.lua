4b31841398f6768e96cbcb44d2af19450ae5e389
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-15 18:21:52 +0100

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,5 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+games { 'gta5' }
 
 file 'v_int_40.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/v_int_40.ytyp'
